37df0395a876c8977be98f989fd160221c796444
mitaskin <mi.taskin027@gmail.com>
2023-07-17 12:54:05 +0300

    Initial Commit

---
+++ SapirmenAPP/SapirmenAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SapirmenAPPApp.swift
+//  SapirmenAPP
+//
+//  Created by Muhammed ikbal TAŞKIN on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SapirmenAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
